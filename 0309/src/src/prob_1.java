86671b6c31ded5cb64779241e17080a284c75609
Administrator <Administrator@sw303_21>
2023-03-10 09:00:32 +0900

    예제1번 완료

---
+++ 0309/src/src/prob_1.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+
+public class prob_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1000 이하의 자연수를 입력하세요: ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 4 == 0) {
+                System.out.print(i + " ");
+                sum += i;
+            }
+        }
+        System.out.println(sum + "입니다.");
+        sc.close();
+    }
+}
